a391e2b632c1809d9ed9c80474713b7acc9f90db
Abdurahman Jafer <95960560+its0beno@users.noreply.github.com>
2023-04-16 15:36:23 +0300

    props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+const firstBook = {
+  author: "Peter Attia MD",
+  image: "./images/book1.jpg",
+  title: "Outlive: The Science and Art of Longevity",
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const secondBook = {
+  author: "Jordan Moore",
+  image:
+    "https://m.media-amazon.com/images/I/51B7kuFwQFL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: "Intersting Facts For Curious Minds",
+};
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        image={firstBook.image}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        image={secondBook.image}
+      />
+    </section>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Booklist />);
